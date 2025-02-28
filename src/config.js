e3b0b515ae9994d074ae36897147761eaaa405fb
chike <davidalumartin@gmail.com>
2025-02-28 20:54:21 +0100

    Initial commit

---
+++ src/config.js
@@ -0,0 +1,7 @@
+// src/config.js
+
+// Local development
+export const API_BASE_URL = 'https://core.cryptowhales-software.com/api';
+
+// When deploying, change this to your shared hosting URL
+// export const API_BASE_URL = 'https://yourdomain.com/wallet-finder/api';
