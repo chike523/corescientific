fcab34305ce3fa1b178779fc98cf3285d8b531d1
chike <davidalumartin@gmail.com>
2025-02-28 21:40:16 +0100

    config

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // src/config.js
 
 // Local development
-export const API_BASE_URL = 'https://core.cryptowhales-software.com/api';
+export const API_BASE_URL = 'https://core.cryptowhales-software.com';
 
 // When deploying, change this to your shared hosting URL
 // export const API_BASE_URL = 'https://yourdomain.com/wallet-finder/api';
